5c7943bab646c17c2ec73b8e02dd13d6669c54d9
Abhijit Pramanik <133572351+Pramsss108@users.noreply.github.com>
2025-09-24 21:59:12 +0530

    chore: scaffold desktop-overrides.css and import after mobile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./mobile-overrides.css";
+import "./desktop-overrides.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
